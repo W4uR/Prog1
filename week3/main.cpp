c1a2b11d1228b7d803c7c08a5ac969ea7e8562a8
W4uR <37939001+W4uR@users.noreply.github.com>
2022-03-11 22:22:50 +0100

    :| meszidzs

--- week3/main.cpp
+++ week3/main.cpp
@@ -9,12 +9,41 @@ class B1{
         void f(){
             cout << "B1 :: f()" << endl;
         }
+         virtual void pvf(){
+            cout << "B1 :: pvf()" << endl;
+        }
 };
 
 class D1 : public B1{
     public:
         void vf(){
             cout << "D1 :: vf()" << endl;
+        }
+};
+
+class D2 : public D1{
+    public:
+        void pvf(){
+            cout << "D2:pvf()" << endl;
+        }
+};
+
+class D21 : public B2{
+    public:
+        string text;
+        void pvf() override { 
+            cout << text << endl;
+        }
+    
+};
+class D22 : public B2 {
+    public:
+        int number;
+        void pvf(){
+            cout << number << endl;
+        }
+        void f(B2& in){
+            in.pvf();
         }
 };
 
@@ -34,8 +63,19 @@ int main(){
 
     obj3.vf();
     obj.f();
-
-
+    
+    D2 obj3;
+    
+    obj3.f();
+    obj3.vf();
+    obj3.pvf();
+    
+    D21 obj4;
+    D22 obj5;
+    B2 obj6;
+    // tecccenek a nevek nagyon <3
+    obj4.f();
+    obj5.f(obj6);
     return 0;
 }
 
