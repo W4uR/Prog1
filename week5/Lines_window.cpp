187d53b706bc2c7190d83702d6dd41f2b183d696
W4uR <37939001+W4uR@users.noreply.github.com>
2022-03-18 14:58:37 +0100

    Update Lines_window.cpp

--- week5/Lines_window.cpp
+++ week5/Lines_window.cpp
@@ -10,7 +10,10 @@ Lines_window::Lines_window(Point xy, int w, int h, const string& title)
     next_y{Point{450,0}, 50, 40, "next y:"},
     xy_out{Point{100,0}, 100, 40, "current (x,y):"},
     //Na ez az új:
-    color_menu{Point{x_max()-70,40},70,20,Menu::vertical,"color"}
+    color_menu{Point{x_max()-70,40},70,20,Menu::vertical,"color"},
+    menu_button{Point{x_max()-80,40},80,20,"Color menu",cb_menu},
+    style_menu{Point{x_max()-70,110},80,20,Menu::vertical,"style"},
+    menu_button{Point{x_max()-80,110},80,20,"Style menu",cb_style_menu}
 {
     attach(next_button);
     attach(quit_button);
@@ -23,6 +26,18 @@ Lines_window::Lines_window(Point xy, int w, int h, const string& title)
     color_menu.attach(new Button{Point{0,0},0,0,"blue",cb_blue});
     color_menu.attach(new Button{Point{0,0},0,0,"black",cb_black});
     attach(color_menu);
+    color_menu.hide();
+    attach(menu_button);
+    
+    style_menu.attach(new Button{Point{0,0},0,0,"Solid",[](Address,Address pw) {reference_to<Lines_window>(pw).change_style(Line_style::solid);}});
+    style_menu.attach(new Button{Point{0,0},0,0,"Dash",[](Address,Address pw) {reference_to<Lines_window>(pw).change_style(Line_style::dash);}});
+    style_menu.attach(new Button{Point{0,0},0,0,"Dot",[](Address,Address pw) {reference_to<Lines_window>(pw).change_style(Line_style::dot);}});
+    style_menu.attach(new Button{Point{0,0},0,0,"Dashdot",[](Address,Address pw) {reference_to<Lines_window>(pw).change_style(Line_style::dashdot);}});
+    style_menu.attach(new Button{Point{0,0},0,0,"Dashdotdot",[](Address,Address pw) {reference_to<Lines_window>(pw).change_style(Line_style::dashdotdot);}});
+    
+    attach(style_menu);
+    style_menu.hide();
+    attach(style_button);
 }
 
 void Lines_window::quit()
