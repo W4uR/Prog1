8cd5c57c96c821d0b93a73a845e1ae000236b81b
Kis Abel <abel.kis45682@gmail.com>
2022-03-28 14:30:41 -0700

    done

--- week5/Lines_window.cpp
+++ week5/Lines_window.cpp
@@ -11,9 +11,9 @@ Lines_window::Lines_window(Point xy, int w, int h, const string& title)
     xy_out{Point{100,0}, 100, 40, "current (x,y):"},
     //Na ez az új:
     color_menu{Point{x_max()-70,40},70,20,Menu::vertical,"color"},
-    menu_button{Point{x_max()-80,40},80,20,"Color menu",cb_menu},
+    color_menu_button{Point{x_max()-100,40},100,40,"Color menu",cb_color_menu},//a sy'mokkal lehetne sy=rakoyni ---hehe angol billentzuyet
     style_menu{Point{x_max()-70,110},80,20,Menu::vertical,"style"},
-    menu_button{Point{x_max()-80,110},80,20,"Style menu",cb_style_menu}
+    style_menu_button{Point{x_max()-100,110},100,40,"Style menu",cb_style_menu}
 {
     attach(next_button);
     attach(quit_button);
@@ -22,22 +22,22 @@ Lines_window::Lines_window(Point xy, int w, int h, const string& title)
     attach(xy_out);
     attach(lines);
 
+    //with callback
     color_menu.attach(new Button{Point{0,0},0,0,"red",cb_red});
     color_menu.attach(new Button{Point{0,0},0,0,"blue",cb_blue});
     color_menu.attach(new Button{Point{0,0},0,0,"black",cb_black});
     attach(color_menu);
     color_menu.hide();
-    attach(menu_button);
-    
+    attach(color_menu_button);
+    //with lambda
     style_menu.attach(new Button{Point{0,0},0,0,"Solid",[](Address,Address pw) {reference_to<Lines_window>(pw).change_style(Line_style::solid);}});
     style_menu.attach(new Button{Point{0,0},0,0,"Dash",[](Address,Address pw) {reference_to<Lines_window>(pw).change_style(Line_style::dash);}});
     style_menu.attach(new Button{Point{0,0},0,0,"Dot",[](Address,Address pw) {reference_to<Lines_window>(pw).change_style(Line_style::dot);}});
     style_menu.attach(new Button{Point{0,0},0,0,"Dashdot",[](Address,Address pw) {reference_to<Lines_window>(pw).change_style(Line_style::dashdot);}});
     style_menu.attach(new Button{Point{0,0},0,0,"Dashdotdot",[](Address,Address pw) {reference_to<Lines_window>(pw).change_style(Line_style::dashdotdot);}});
-    
     attach(style_menu);
     style_menu.hide();
-    attach(style_button);
+    attach(style_menu_button);
 }
 
 void Lines_window::quit()
@@ -67,4 +67,10 @@ void Lines_window::cb_blue(Address,Address pw){
 }
 void Lines_window::cb_black(Address,Address pw){
     reference_to<Lines_window>(pw).black_pressed();
+}
+void Lines_window::cb_color_menu(Address,Address pw){
+    reference_to<Lines_window>(pw).color_menu_pressed();
+}
+void Lines_window::cb_style_menu(Address,Address pw){
+    reference_to<Lines_window>(pw).style_menu_pressed();
 }
--- week5/Lines_window.h
+++ week5/Lines_window.h
@@ -4,19 +4,33 @@ using namespace Graph_lib;
 
 struct Lines_window : Graph_lib::Window {
 	Lines_window(Point xy, int w, int h, const string& title );
-	
+
 	Open_polyline lines;
 	Menu color_menu;
-	
+	Button color_menu_button;
+	Menu style_menu;
+	Button style_menu_button;
+
+    static void cb_color_menu(Address,Address);
+	static void cb_style_menu(Address,Address);
+
 	static void cb_red(Address,Address);
 	static void cb_blue(Address,Address);
 	static void cb_black(Address,Address);
-	
-	void red_pressed(){change(Color::red); redraw();}
-	void blue_pressed(){change(Color::blue); redraw();}
-	void black_pressed(){change(Color::black); redraw();}
-	void change(Color c){lines.set_color(c);}
-	
+
+	void red_pressed(){change_color(Color::red); redraw();}
+	void blue_pressed(){change_color(Color::blue); redraw();}
+	void black_pressed(){change_color(Color::black); redraw();}
+	void change_color(Color c){lines.set_color(c);hide_color_menu();}
+
+    void change_style(Line_style ls){lines.set_style(ls); hide_style_menu(); redraw();}
+
+    void hide_color_menu(){color_menu.hide(); color_menu_button.show();}
+	void hide_style_menu(){style_menu.hide(); style_menu_button.show();}
+
+	void color_menu_pressed(){color_menu_button.hide();color_menu.show();}
+	void style_menu_pressed(){style_menu_button.hide();style_menu.show();}
+
 	bool wait_for_button();
 
 private:
