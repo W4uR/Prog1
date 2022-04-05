154daeb69d32ec7be55ba6f9fd6c55ebe1b955db
Kis Abel <abel.kis45682@gmail.com>
2022-04-05 13:14:46 -0700

    done

--- week8/double_vector.cpp
+++ week8/double_vector.cpp
@@ -1,13 +1,5 @@
 #include "../std_lib_facilities.h"
 
-istream& operator>>(istream& is, vector<double>& vec)
-{
-    double d;
-    is >> d;
-    vec.push_back(d);
-    return is;
-}
-
 ostream& operator<<(ostream& os, const vector<double>& vec)
 {
     for(int i = 0; i < vec.size(); ++i)
@@ -17,10 +9,21 @@ ostream& operator<<(ostream& os, const vector<double>& vec)
 
 template<typename T1, typename T2>
 void output_vector_pairs(const vector<T1>& v1,const vector<T2>& v2){
-    
     for(int i = 0; i < v1.size(); i++){
-        cout << v1[i] << " - " << v2[i] <<endl;
+        cout <<i+1<<"\t" <<setw(6)<<v1[i] << "\t-" << setw(6)<<v2[i] <<endl;
     }
+}
+
+// az accumulate-el "nem mukodott" a vd. A decimal elveszett
+template<typename T>
+T my_sum(const vector<T>& _vector){
+    T sum = T();
+
+    for(auto & i : _vector){
+        sum += i;
+    }
+
+    return sum;
 }
 
 int main(){
@@ -29,8 +32,11 @@ int main(){
 
 
     ifstream readFile("input2.txt");
+    double value;
+    readFile >> value;
     while(!readFile.eof()){
-        readFile >> vd;
+        vd.push_back(value);
+        readFile >> value;
     }
     readFile.close();
     cout << vd;
@@ -39,12 +45,24 @@ int main(){
     copy(vd.begin(),vd.end(),vi.begin());
     output_vector_pairs(vd,vi);
 
-    int vi_sum = accumulate(vi.begin(),vi.end(),0);
-    double vd_sum = accumulate(vd.begin(),vd.end(),0);
+    int vi_sum = my_sum(vi);
+    double vd_sum = my_sum(vd);
 
     cout << vd_sum << endl;
-    //cout << vi_sum << endl;
-    cout << vd_sum - (double)vi_sum << endl; // not gud
+    cout << vi_sum << endl;
+    cout << vd_sum - vi_sum << endl<<endl;
 
+    reverse(vd.begin(),vd.end());
+    cout << vd;
+    double mean_val = vd_sum / vd.size();
+    cout << "mean value of vd is: " << mean_val << endl;
+
+    vector<double> vd2;
+    copy_if(vd.begin(),vd.end(),back_inserter(vd2),
+    [mean_val](double x){
+        return x < mean_val;
+    });
+    sort(vd.begin(),vd.end());
+    cout << vd;
     return 0;
 }
